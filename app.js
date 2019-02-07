49d5fba0bd1190fd0ee234cba0aa29f092f052b0
xaviguasch <xaviguasch@gmail.com>
2019-02-07 11:35:03 +0100

    adding data to mailchimp servers

--- app.js
+++ app.js
@@ -19,8 +19,36 @@ app.post('/', function (req, res) {
     const lastName = req.body.lName
     const email = req.body.email
 
-    console.log(firstName, lastName, email);
+    const data = {
+        members: [{
+            email_address: email,
+            status: 'subscribed',
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    }
 
+    const jsonData = JSON.stringify(data)
+
+    const options = {
+        url: 'https://us20.api.mailchimp.com/3.0/lists/1168c77b67',
+        method: 'POST',
+        headers: {
+            'Authorization': 'xavi1 5dfd45f040d7222aaae0efb5b994a770-us20'
+        },
+        body: jsonData
+
+    }
+
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(response.statusCode);
+        }
+    })
 
 
 })
@@ -29,3 +57,9 @@ app.post('/', function (req, res) {
 app.listen(3000, function () {
     console.log('Server is running on port 3000');
 })
+
+// API key
+// 5dfd45f040d7222aaae0efb5b994a770-us20
+
+// list id
+// 1168c77b67
